7df008aba2f3c980165893c68d66923f80c07671
David Kesler <keslerdr@gmail.com>
2022-08-29 20:57:13 -0400

    Adding dnd1-7, works as is

---
+++ src/test/kotlin/integration/Dnd1_7.kt
@@ -0,0 +1,8 @@
+package integration
+
+import SolverConfiguration
+
+class Dnd1_7 : BaseIntegrationTest() {
+    override val file = "/dnd1-7"
+    override val config = SolverConfiguration(false)
+}
