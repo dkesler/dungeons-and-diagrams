8b7db07f302c335443044b7b20696ae53b4f1723
David Kesler <keslerdr@gmail.com>
2022-09-01 19:23:23 -0400

    Updating ExtendHallway to use rule.each

--- src/main/kotlin/rules/ExtendHallway.kt
+++ src/main/kotlin/rules/ExtendHallway.kt
@@ -2,32 +2,29 @@ package rules
 
 import game.Board
 import game.CellType
+import game.TypeRange
+import utils.Point
 
 class ExtendHallway : Rule {
     override fun name() = "ExtendHallway"
     override fun apply(board: Board): ApplyResult {
-        for (row in board.grid.rows) {
-            for (col in board.grid.cols) {
-                if (board.grid.cells[row][col].eq(CellType.HALL)) {
-                    val neighbors = board.grid.neighbors(row, col)
-                    //if a hallway ever has neighbors.count()-1 walls, it would be a dead end.  so if it has neighbors.count()-2 walls
-                    //as neighbors, and it has more neighbors that might be walls but aren't known, those neighbors can't be walls
-                    if (neighbors.count{it.type.eq(CellType.WALL) } == neighbors.count()-2 &&
-                        neighbors.count{it.type.canBe(CellType.WALL)} > neighbors.count()-2) {
-                        val unknownNeighbors = neighbors.filter{it.type.canBe(CellType.WALL) && !it.type.known }
-                        var b = board
-                        for (n in unknownNeighbors) {
-                            val update = b.update(n.row, n.col, n.type.types - CellType.WALL)
-                            if (!update.valid) {
-                                return ApplyResult(true, true, name(), "${name()}.row[$row]col[${col}]", b)
-                            }
-                            b = update.board
-                        }
-                        return ApplyResult(true, false, name(), "${name()}.row[$row]col[${col}]", b)
-                    }
-                }
+        fun rule(point: Point): Rule.Check? {
+            val neighbors = board.grid.neighbors(point.row, point.col)
+            //if a hallway ever has neighbors.count()-1 walls, it would be a dead end.  so if it has neighbors.count()-2 walls
+            //as neighbors, and it has more neighbors that might be walls but aren't known, those neighbors can't be walls
+            if (neighbors.count{it.type.eq(CellType.WALL) } == neighbors.count()-2 &&
+                neighbors.count{it.type.canBe(CellType.WALL)} > neighbors.count()-2) {
+                val toUpdate = neighbors.filter{it.type.canBe(CellType.WALL) && !it.type.known }
+                    .map{ Point(it.row, it.col, TypeRange(it.type.types - CellType.WALL)) }
+                return Rule.Check(board.update(toUpdate), ".row[${point.row}]col[${point.col}]")
             }
+            return null
         }
-        return ApplyResult(false, false, name(), "", board)
+
+        return each(
+            board,
+            { it.type.eq(CellType.HALL) },
+            ::rule
+        )
     }
 }
