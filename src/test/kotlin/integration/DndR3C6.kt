05725ea6d7cc653ce2bdf00e700733fd3f42e702
David Kesler <keslerdr@gmail.com>
2022-09-09 22:01:38 -0400

    Adding dnd3-6.  Works as is

---
+++ src/test/kotlin/integration/DndR3C6.kt
@@ -0,0 +1,8 @@
+package integration
+
+import SolverConfiguration
+
+class DndR3C6 : BaseIntegrationTest() {
+    override val file = "/dnd3-6"
+    override val config = SolverConfiguration(false)
+}
