7df0773d990f69e40907633fb078969624db41fc
David Kesler <keslerdr@gmail.com>
2022-09-09 21:55:04 -0400

    Adding dnd3-4.  Works as is

---
+++ src/test/kotlin/integration/DndR3C4.kt
@@ -0,0 +1,8 @@
+package integration
+
+import SolverConfiguration
+
+class DndR3C4 : BaseIntegrationTest() {
+    override val file = "/dnd3-4"
+    override val config = SolverConfiguration(false)
+}
