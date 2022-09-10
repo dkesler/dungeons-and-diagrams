d862434029ed2ab9374e7a52f4fb5813babe175b
David Kesler <keslerdr@gmail.com>
2022-09-09 21:48:13 -0400

    Adding dnd3-2.  Works as is

---
+++ src/test/kotlin/integration/DndR3C2.kt
@@ -0,0 +1,8 @@
+package integration
+
+import SolverConfiguration
+
+class DndR3C2 : BaseIntegrationTest() {
+    override val file = "/dnd3-2"
+    override val config = SolverConfiguration(false)
+}
