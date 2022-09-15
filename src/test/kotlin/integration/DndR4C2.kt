457532ef5d30123f35f5eaa1be496291fba291af
David Kesler <keslerdr@gmail.com>
2022-09-15 18:10:39 -0400

    Adding dnd 4-2, works as is

---
+++ src/test/kotlin/integration/DndR4C2.kt
@@ -0,0 +1,8 @@
+package integration
+
+import SolverConfiguration
+
+class DndR4C2 : BaseIntegrationTest() {
+    override val file = "/dnd4-2"
+    override val config = SolverConfiguration(false)
+}
