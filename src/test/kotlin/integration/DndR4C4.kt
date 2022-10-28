15449c5829a121974a34bc4bebf94ceaa9eebaf0
David Kesler <keslerdr@gmail.com>
2022-10-28 17:20:21 -0400

    Adding dnd 4-4, doesn't work without bifurcation

---
+++ src/test/kotlin/integration/DndR4C4.kt
@@ -0,0 +1,8 @@
+package integration
+
+import SolverConfiguration
+
+class DndR4C4 : BaseIntegrationTest() {
+    override val file = "/dnd4-4"
+    override val config = SolverConfiguration(true)
+}
